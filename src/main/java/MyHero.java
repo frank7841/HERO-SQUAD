db4da9863e3eed1c7397327f5817044e014fd3ba
frank7841 <franklinewax@gmail.com>
2020-07-07 07:37:31 +0300

    Creating private objects name age special powe and weakness

--- src/main/java/MyHero.java
+++ src/main/java/MyHero.java
@@ -1,4 +1,8 @@
 import java.util.ArrayList;
 public class MyHero {
-
+    private String name;
+    private int age;
+    private String specialPower;
+    private String weakness;
+    private static ArrayList<MyHero> instances = new ArrayList<>();
 }
