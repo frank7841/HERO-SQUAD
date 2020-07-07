ed8dbe02d448d1d61e7af4e2a2a3c40dff607a88
frank7841 <franklinewax@gmail.com>
2020-07-07 07:25:28 +0300

    Importing Spark dependencies and creating main class App with main method

---
+++ src/main/java/App.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import static spark.Spark.*;
+public class App {
+    public static void main(String[] args){
+        staticFileLocation("/public");
+    }
+}
