990de4589806de7be47824af11e42a00e8877147
frank7841 <franklinewax@gmail.com>
2020-07-07 21:03:51 +0300

    created and desined success handler to allow user to view created squads

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,27 +5,19 @@ import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 import static spark.Spark.*;
 public class App {
-    public static void main(String[] args){
-        staticFileLocation("/public");
 
-        post("/heroform/new", (request, response) -> {
+    public static void main(String[] args){
+
+        staticFileLocation("/public");
+        post("/heroform", (request, response) -> {
             Map<String, Object> model = new HashMap<>();
             String name = request.queryParams("name");
             int age = Integer.parseInt(request.queryParams("age"));
             String power = request.queryParams("specialPower");
             String weakness = request.queryParams("weakness");
             MyHero newMember = new MyHero(name, age, power,weakness);
-            model.put("hero", MyHero.getAll());
+            model.put("heros", MyHero.getAll());
             return new ModelAndView(model, "herosucess.hbs");
         }, new HandlebarsTemplateEngine());
-
-        get("/heroform/new", (request, response) -> {
-            Map<String, Object> model = new HashMap<String, Object>();
-            return new ModelAndView(model, "heroform.hbs");
-        }, new HandlebarsTemplateEngine());
-
-
-
     }
-
 }
