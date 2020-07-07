c0d067b5d498bb83ddcfaaf2fa22caf0b46774fb
frank7841 <franklinewax@gmail.com>
2020-07-07 08:21:29 +0300

    Created a findById method to return a Specific Hero By Id

--- src/main/java/MyHero.java
+++ src/main/java/MyHero.java
@@ -7,27 +7,40 @@ public class MyHero {
     private static ArrayList<MyHero> instances = new ArrayList<>();
     private int id;
 
-    public MyHero(String name,int age, String specialPower, String weakness){
+    public MyHero(String name, int age, String specialPower, String weakness) {
         this.name = name;
         this.age = age;
         this.specialPower = specialPower;
-        this.weakness=weakness;
+        this.weakness = weakness;
         instances.add(this);
         this.id = instances.size();
     }
 
-    public String getName() { return name; }
+    public String getName() {
+        return name;
+    }
 
-    public int getAge() { return age; }
+    public int getAge() {
+        return age;
+    }
 
-    public String getSpecialPower() {return specialPower; }
+    public String getSpecialPower() {
+        return specialPower;
+    }
 
-    public String getWeakness() { return weakness; }
+    public String getWeakness() {
+        return weakness;
+    }
 
-    public int getId() { return id; }
+    public int getId() {
+        return id;
+    }
 
     public static ArrayList<MyHero> getAll() {
         return instances;
     }
-}
 
+    public static MyHero findById(int id) {
+        return instances.get(id - 1);
+    }
+}
