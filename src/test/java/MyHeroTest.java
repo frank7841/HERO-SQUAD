df13b8af8e5042c6650c792826220f81e6ecbeab
frank7841 <franklinewax@gmail.com>
2020-07-07 07:34:55 +0300

    Creating a method that checks if MyHero class Instanciates properly

--- src/test/java/MyHeroTest.java
+++ src/test/java/MyHeroTest.java
@@ -1,5 +1,13 @@
 import junit.framework.TestCase;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class MyHeroTest extends TestCase {
 
+    @Test
+    public void checkingWhetherItInstantiatesProperly() {
+        MyHero hero = new MyHero("Batman", 27, "I am Batman", "Nothing");
+        assertEquals(true, hero instanceof MyHero);
+    }
 }
