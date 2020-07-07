17aec361793da8c7bbe1727eb5577e15dc5087c2
frank7841 <franklinewax@gmail.com>
2020-07-07 07:47:23 +0300

    Creating Test to get hero name input

--- src/test/java/MyHeroTest.java
+++ src/test/java/MyHeroTest.java
@@ -10,4 +10,9 @@ public class MyHeroTest  {
         MyHero hero = new MyHero("Batman", 27, "I am Batman", "Nothing");
         assertEquals(true, hero instanceof MyHero);
     }
+    @Test
+    public void getsNameInput() {
+        MyHero hero = new MyHero("Batman", 27, "I am Batman", "Nothing");
+        assertEquals("Superman", hero.getName());
+    }
 }
