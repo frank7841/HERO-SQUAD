79577a4f3995f7a73e4a2878ea0ff72fd750a474
frank7841 <franklinewax@gmail.com>
2020-07-07 08:45:40 +0300

    Definingg class MySquad

--- src/main/java/MySquad.java
+++ src/main/java/MySquad.java
@@ -1,3 +1,16 @@
 import java.util.ArrayList;
 public class MySquad {
+    private final int size;
+    private final String squadName;
+    private final String purpose;
+    private static ArrayList<MySquad> instances = new ArrayList<MySquad>();
+    private int id;
+
+    public MySquad(int size, String squadName, String purpose){
+        this.size=size;
+        this.squadName=squadName;
+        this.purpose =purpose;
+        instances.add(this);
+        this.id = instances.size();
+    }
 }
--- src/test/java/MySquadTest.java
+++ src/test/java/MySquadTest.java
@@ -9,4 +9,9 @@ public class MySquadTest  {
         MySquad squad = new MySquad(10,"Justice League", "Fight Crime");
         assertEquals(true, squad instanceof MySquad);
     }
+    @Test
+    public void getsSquadSize() {
+        MySquad squad = new MySquad(10,"Justice League", "Protect citizens");
+        assertEquals(10, squad.getSize());
+    }
 }
