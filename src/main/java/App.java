571287e492e60132007f89769bd0b1249d334a41
frank7841 <franklinewax@gmail.com>
2020-07-08 08:25:13 +0300

    Created pom.xml

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,9 +5,16 @@ import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 import static spark.Spark.*;
 public class App {
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
+    public static void main(String[] args) {
 
-    public static void main(String[] args){
-
+        port(getHerokuAssignedPort());
         staticFileLocation("/public");
         get("/", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
